f5f9c9f886977c198e207f31a70dddacc07633d5
Shashank <shashank00098@gmail.com>
2022-11-25 09:34:15 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React, {useState} from "react";
 import "./styles.css";
-var colors = "red";
-var col = "green";
-const box = { backgroundColor: colors, color: col };
-var userName = prompt("Give me your name");
+
 export default function App() {
+  var colors = "green";
+  var col = "red";
+  const box = { backgroundColor: colors, color: col };
+  var userName = "Shashank"; //prompt("Give me your name");
+  const [likeCounter, setLikeCounter] = useState(0);
+  function likeClickHandler() {
+    var newLikeCounterValue = likeCounter + 2;
+        setLikeCounter(newLikeCounterValue);
+  }
+  console.log("Clickd  ", likeCounter);
+
   return (
     <div className="App">
       <h1 style={box}>Welcome{userName}</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <button onClick={likeClickHandler}>LikeMe! </button> {likeCounter}
     </div>
   );
 }
+
