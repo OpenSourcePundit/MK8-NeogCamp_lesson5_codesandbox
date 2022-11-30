3e21a5055cd07a1c29f0dc73965dca75f2028472
Shashank <shashank00098@gmail.com>
2022-11-30 07:44:38 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,23 +48,3 @@ export default function App() {
     </div>
   );
 }
-===
-iport React,{useState} from "react";
-import "./styles.css";
-
-export default function App(){
-  var [userInput,setUserInput]= useState("");
-  function onChangeHandler(event){
-     setUserInput(event.target.value);
-    
-  }
-  return (
-    <div className="App">
-      <h1>Inside Out !! </h1>
-      <input onChange={onChangeHandler}/>
-      <h2>Start editing to see some magic happen!</h2> 
-      <h3>Welcome  {userInput}</h3>   
->>>>>>> 1a34b768f96b9ebd89ea2fcacdf9e22e0d6f478e
-    </div>
-  );
-  }
